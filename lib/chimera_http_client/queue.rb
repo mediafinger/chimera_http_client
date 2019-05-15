ed10b07aa32200f11e4aa0193a3920ec35352808
Andreas Finger <webmaster@mediafinger.com>
2019-05-16 00:57:52 +0200

    Extract functionality to Queue < Connection

---
+++ lib/chimera_http_client/queue.rb
@@ -0,0 +1,49 @@
+module ChimeraHttpClient
+  class Queue < Connection
+    def add(method, endpoint, options = {})
+      http_method = method.downcase.to_sym
+      options[:body_optional] = true if %i[get delete].include?(http_method)
+
+      body = extract_body(options)
+      headers = extract_headers(options, default_headers)
+
+      req = Request.new(logger: @logger).create(
+        url: url(endpoint),
+        method: http_method,
+        body: body,
+        options: augmented_options(options),
+        headers: headers
+      )
+
+      queued_requests << req
+    end
+
+    def execute
+      queued_requests.each do |request|
+        hydra.queue(request.request)
+      end
+
+      hydra.run
+
+      responses = queued_requests.map { |request| request.result }
+
+      empty
+
+      responses
+    end
+
+    def empty
+      @queued_requests = []
+    end
+
+    def queued_requests
+      @queued_requests ||= []
+    end
+
+    private
+
+    def hydra
+      @hydra ||= Typhoeus::Hydra.new
+    end
+  end
+end
---
+++ spec/chimera_http_client/queue_spec.rb
@@ -0,0 +1,59 @@
+require "spec_helper"
+
+describe ChimeraHttpClient::Queue do
+  let(:queue) { described_class.new(base_url: base_url) }
+  let(:base_url) { "http://127.0.0.1:3000/v1" }
+
+  let(:method)       { "GET" }
+  let(:endpoint)     { "dummy" }
+  let(:bad_endpoint) { "failure" }
+
+  let(:typhoeus_response) { Typhoeus::Response.new(code: response_code, body: response_json, total_time: response_time) }
+  let(:response_code) { 200 }
+  let(:response_body) { { "id" => 42, "name" => "Andy" } }
+  let(:response_json) { response_body.to_json }
+  let(:response_time) { 0.5 }
+
+  let(:failure_response) { Typhoeus::Response.new(code: failure_code, body: failure_body, total_time: response_time) }
+  let(:failure_code) { 404 }
+  let(:failure_body) { "#{failure_code} not_found" }
+
+  describe "#add" do
+    it { expect(queue).to be_a(described_class) }
+    it { expect(queue.add(method, endpoint)).to be_a(Array) }
+    it { expect(queue.add(method, endpoint).first).to be_a(ChimeraHttpClient::Request) }
+  end
+
+  describe "#execute" do
+    before do
+      Typhoeus.stub("#{base_url}/#{endpoint}").and_return(typhoeus_response)
+      Typhoeus.stub("#{base_url}/#{bad_endpoint}").and_return(failure_response)
+    end
+
+    it "runs all queued requests", :aggregated_failures do
+      queue.add(method, endpoint)
+      queue.add(method, bad_endpoint)
+
+      responses = queue.execute
+
+      expect(responses).to be_a(Array)
+      expect(responses.count).to eq(2)
+
+      expect(responses.first).to be_a(ChimeraHttpClient::Response)
+      expect(responses.first.code).to eq(200)
+      expect(responses.first.parsed_body).to eq(response_body)
+
+      expect(responses.last).to be_a(ChimeraHttpClient::Error)
+      expect(responses.last.code).to eq(404)
+      expect(responses.last.parsed_body).to eq({ "non_json_body" => failure_body })
+    end
+  end
+
+  describe "#empty" do
+    it "deletes queued requests" do
+      queue.add(method, endpoint)
+
+      expect(queue.empty).to eq([])
+    end
+  end
+end
